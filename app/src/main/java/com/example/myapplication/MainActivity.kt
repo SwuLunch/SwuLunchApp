1a40e3606bc3b98404d33995e10a46cb26eff41e
Yeji9 <poiuy5330@naver.com>
2021-02-04 16:29:53 +0900

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.content.Intent
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var edtId : EditText
+    lateinit var edtPwd : EditText
+    lateinit var btnLogin : Button
+    lateinit var btnUser : Button
+    lateinit var dbswu : SQLiteDatabase
+    lateinit var myHelper : myDBHelper
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_main)
+
+        //로그인 화면
+        edtId = findViewById(R.id.edtID)
+        edtPwd = findViewById(R.id.edtPwd)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnUser = findViewById(R.id.btnUser)
+        myHelper = myDBHelper(this)
+
+
+        btnLogin.setOnClickListener {
+            dbswu = myHelper.readableDatabase
+            var cursor : Cursor
+            cursor = dbswu.rawQuery("SELECT * FROM USERS;", null)
+
+            var id = edtId.text.toString()
+            var pwd = edtPwd.text.toString()
+            var login = false
+
+            while (cursor.moveToNext()) {
+                if(id == cursor.getString(0)
+                    && pwd == cursor.getString(1)) {
+                    var intent = Intent(this, HomeActivity::class.java)
+                    intent.putExtra("nickname", cursor.getString(2).toString())
+                    startActivity(intent)
+                    login = true
+                    break
+                    }
+            }
+
+            if(login == false)
+            {
+                Toast.makeText(this, "회원이 아닙니다", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        btnUser.setOnClickListener {
+            var intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+        /*
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController) */
+    }
+
+    inner class myDBHelper(context: Context) : SQLiteOpenHelper(context, "SWU", null, 1) {
+        override fun onCreate(db: SQLiteDatabase?) {
+            db!!.execSQL("CREATE TABLE USERS ( ID CHAR(20) PRIMARY KEY, PWD CHAR(20), NICKNAME CHAR(10));")
+        }
+
+        override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+            db!!.execSQL("DROP TABLE IF EXISTS SWU")
+            onCreate(db)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/RegisterActivity.kt
@@ -0,0 +1,56 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity() {
+
+    lateinit var edtId2 : EditText
+    lateinit var edtPwd2 : EditText
+    lateinit var edtNickname : EditText
+    lateinit var btnUser2 : Button
+    lateinit var dbswu : SQLiteDatabase
+    lateinit var myHelper : myDBHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_regis)
+
+        //회원가입 화면
+        edtId2 = findViewById(R.id.edtID2)
+        edtPwd2 = findViewById(R.id.edtPwd2)
+        edtNickname = findViewById(R.id.edtNickname)
+        btnUser2 = findViewById(R.id.btnUser2)
+        myHelper = myDBHelper(this)
+
+        btnUser2.setOnClickListener {
+            dbswu = myHelper.writableDatabase
+            dbswu.execSQL("INSERT INTO USERS VALUES('" + edtId2.text.toString() + "', '"
+                    + edtPwd2.text.toString() + "', '" + edtNickname.text.toString() + "');")
+
+            Toast.makeText(this, "회원가입이 되었습니다", Toast.LENGTH_SHORT).show()
+
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    inner class myDBHelper(context: Context) : SQLiteOpenHelper(context, "SWU", null, 1) {
+        override fun onCreate(db: SQLiteDatabase?) {
+            db!!.execSQL("CREATE TABLE USERS ( ID CHAR(20) PRIMARY KEY, PWD CHAR(20), NICKNAME CHAR(10));")
+        }
+
+        override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+            db!!.execSQL("DROP TABLE IF EXISTS SWU")
+            onCreate(db)
+        }
+    }
+}
